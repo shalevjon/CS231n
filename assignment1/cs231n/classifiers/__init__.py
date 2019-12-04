5e3ffacdfc9743fc69e266eba04bdeb2ce5f4b66
shalevjon <shalevjon@gmail.com>
2019-12-05 00:10:31 +0200

    uploaded assignment 1 files

---
+++ assignment1/cs231n/classifiers/__init__.py
@@ -0,0 +1,5 @@
+from cs231n.classifiers.k_nearest_neighbor import *
+from cs231n.classifiers.linear_classifier import *
+
+if __name__ == '__main__':
+    knn = k_nearest_neighbor.KNearestNeighbor()
